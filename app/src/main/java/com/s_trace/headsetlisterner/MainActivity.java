34759983e3316c3f92d779835fa7ee15ba9dcb03
S-trace <S-trace@list.ru>
2018-01-27 06:02:22 +0300

    [LOG] MainActivity: Log onKeyDown, onKeyUp and onKeyLongPress events handling

--- app/src/main/java/com/s_trace/headsetlisterner/MainActivity.java
+++ app/src/main/java/com/s_trace/headsetlisterner/MainActivity.java
@@ -2,10 +2,12 @@ package com.s_trace.headsetlisterner;
 
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -24,4 +26,32 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        Log.d(TAG, "onKeyDown: Got keycode=" + keyCode + ", event=" + event);
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        Log.d(TAG, "onKeyUp: Got keycode=" + keyCode + ", event=" + event);
+        if ((event.getFlags() & KeyEvent.FLAG_CANCELED) != 0) {
+            Log.d(TAG, "onKeyUp: ignoring canceled KeyEvent");
+            return super.onKeyUp(keyCode, event);
+        }
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_HEADSETHOOK:
+                Log.d(TAG, "onKeyUp: keyevent.KEY_PTT_RELEASE");
+                Intent intent = new Intent("keyevent.KEY_PTT_RELEASE");
+                sendBroadcast(intent);
+                return true;
+        }
+        return super.onKeyUp(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyLongPress(int keyCode, KeyEvent event) {
+        Log.d(TAG, "onKeyLongPress: Got keycode=" + keyCode + ", event=" + event);
+        return super.onKeyLongPress(keyCode, event);
+    }
 }
